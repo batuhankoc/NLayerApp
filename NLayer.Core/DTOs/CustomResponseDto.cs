6a13a150673a84e06db53c8d28c1cb18a7083d83
batuhankoc <batuhankocc98@gmail.com>
2022-07-18 01:59:41 +0300

    01:59

--- NLayer.Core/DTOs/CustomResponseDto.cs
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -10,6 +10,7 @@ namespace NLayer.Core.DTOs
     public class CustomResponseDto<T>
     {
         public T Data { get; set; }
+        //
         [JsonIgnore]
         public int StatusCode { get; set; }
         public List<String> Errors { get; set; }
