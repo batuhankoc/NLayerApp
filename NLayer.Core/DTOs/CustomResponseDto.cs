0b89ce3b8b72c2c32edf420cda04bf8bf00b712e
batuhankoc <batuhankocc98@gmail.com>
2022-07-18 01:54:26 +0300

    first try

--- NLayer.Core/DTOs/CustomResponseDto.cs
+++ NLayer.Core/DTOs/CustomResponseDto.cs
@@ -10,7 +10,7 @@ namespace NLayer.Core.DTOs
     public class CustomResponseDto<T>
     {
         public T Data { get; set; }
-
+        //deneme
         [JsonIgnore]
         public int StatusCode { get; set; }
         public List<String> Errors { get; set; }
