d92d4a2a5e3757b9a9d9d12b6bfc8cab52ef3d35
batuhankoc <batuhankocc98@gmail.com>
2022-07-18 14:52:04 +0300

    FluentValidations

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NLayer.API.Filters;
 using NLayer.Core.DTOs;
 using NLayer.Core.Models;
 using NLayer.Core.Services;
@@ -10,21 +11,19 @@ namespace NLayer.API.Controllers
     public class ProductsController : CustomBaseController
     {
         private readonly IMapper _mapper;
-        private readonly IService<Product> _service;
-        private readonly IProductService productService;
+        private readonly IProductService _service;
 
         public ProductsController(IMapper mapper, IService<Product> service, IProductService productService)
         {
             _mapper = mapper;
-            _service = service;
-            this.productService = productService;
+            _service = productService;
         }
 
         //[HttpGet("GetProductsWithCategory")]
         [HttpGet("[action]")]
         public async Task<IActionResult> GetProductsWithCategory()
         {
-            return CreateActionResult(await productService.GetProductWithCategory());
+            return CreateActionResult(await _service.GetProductWithCategory());
         }
 
         [HttpGet]
