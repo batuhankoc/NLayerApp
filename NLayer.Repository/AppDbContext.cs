87095d1b75ac68c0b32f8ab5719a937afea95759
batuhankoc <batuhankocc98@gmail.com>
2022-07-18 13:48:14 +0300

    ProductController CategoryController

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -15,7 +15,7 @@ namespace NLayer.Repository
         {
 
         }
-        public DbSet<ProductFeature> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductFeature> ProductFeatures { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
